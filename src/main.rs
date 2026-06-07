81cf4caaa15ba4389a8211ff84852f8b3d60c409
TheToastiest <rehelvin@gmail.com>
2026-06-06 21:39:10 -0400

    Rust-lang Ported Version of riftnet. Updated for multiplexing client swarm testing, left TODOs for future phases.

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-// There is no central main.rs.
-// To test please run the server from cargo run --bin server --release
-// then run the client from cargo run --bin client --release
-
-
-fn main() {
-    println!("There is no central main.rs.To test please run the server from cargo run --bin server --release then run the client from cargo run --bin client --release");
-}
